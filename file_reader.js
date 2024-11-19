90758829f92e4e7b23cbcd6cf351127bc0bb1e70
Christian Jimenez <cajt@outlook.com>
2024-11-19 11:59:17 -0800

    Done

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,13 @@
 const fs = require("fs");
+const { json } = require("stream/consumers");
 
-fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
+fs.readFile("./firstname.txt", "utf-8", (err, Fname) => {
+  fs.readFile("./lastname.txt", "utf-8", (err, Lname) =>{
+    fs.readFile("./age.txt", "utf-8", (err, age) => {
+      fs.readFile("./hobbies.txt", "utf-8", (err, hobbies) => {
+        arr = JSON.parse(hobbies)
+        console.log(`${Fname} ${Lname} is ${age} years old and his hoobies are ${arr[0]} and ${arr[1]}`)
+      })
+    })
+  })
 })
